6d88b130417b37eb216d5a6a709ecce92e3ea110
Paula <psanzilundain@gmail.com>
2024-02-22 12:33:30 +0100

    Corrección ejercicio

--- main.js
+++ main.js
@@ -6,22 +6,27 @@ const message = document.querySelector('.js-message');
 const counter = document.querySelector('.js-counter');
 let accumulator = 0;
 
+const restartButton = document.querySelector('.js-button-restart');
+
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-const randomNumber = getRandomNumber(100);
+let randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
 function rules(){
-    let inputValue = inputNumber.value;
+    let inputValue = parseInt(inputNumber.value);
+
+    //EN EL INPUT VALUE ME HA FALTADO PONERLE UN PARSEINT() PORQUE RECUERDA QUE LOS VALORES QUE VIENEN DEL HTML SON STRINGS, EN ESTE CASO HA FUNCIONADO PERO EN OTRO TE PUEDE DAR PROBLEMAS!!!!!!!
+
     if (inputValue < 0 || inputValue > 100 || inputValue % 1 !== 0){
         message.innerHTML = `El número tiene que estar entre 0 y 100.`
     }else if (inputValue > randomNumber){
         message.innerHTML = `Demasiado alto.`
     }else if (inputValue < randomNumber){
         message.innerHTML = `Demasiado bajo.`
-    }else if (inputValue = randomNumber){
+    }else if (inputValue === randomNumber){
         message.innerHTML = `Has ganado, campeona!!!!!!`
     }
 }
@@ -39,4 +44,21 @@ function handleRandom(event){
 }
 
 
+//con esta nueva función manejadora hacemos que con la misma función se cree otro número aleatorio al clickar en el botón volver a empezar. Entonces al darle a volver a empezar ese nuevo número aleatorio es el que va a utilizar para seguir con el juego. Estamos reasignando el valor de la variable randomNumber.
+
+const handleRestart = (event) => {
+    event.preventDefault();
+    counter.innerHTML = 'Número de intentos: 0';
+    message.innerHTML = 'Pista: Escribe el número y dale a Prueba';
+    inputNumber.value = '';
+
+//Con estas 3 líneas de código de counter, message e inputNumber reseteamos su valores al clickar en volver a empezar <3
+
+
+randomNumber = getRandomNumber(100);
+console.log(`Prueba: ${randomNumber}`);
+}
+
+
 button.addEventListener('click', handleRandom);
+restartButton.addEventListener('click', handleRestart);
